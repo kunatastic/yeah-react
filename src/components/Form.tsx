e5b89375d8f8fbde3f32644edbd483c4816d54c2
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-03-19 01:07:11 +0530

    level-03 init

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { formFields } from "../data/FormField";
-import { IFormFieldProps } from "../types/forms";
+import { IFormFieldProps, IFormFields, IFormInputComponentProps } from "../types/forms";
 
 function Form(props: IFormFieldProps) {
   const { closeFormCB, showResultsCB } = props;
@@ -33,41 +33,31 @@ function Form(props: IFormFieldProps) {
     );
   }
 
+  // TODO: Types
+  const onChangeHandler = (e: any) =>
+    setFormField(
+      formField.map((field) => {
+        if (field.id === id) return { ...field, value: e.target.value };
+        return field;
+      })
+    );
+
+  // TODO: Types
+  const onClickHandler = () => {
+    removeField(id);
+  };
+
   return (
     <>
       <form onSubmit={handleFormSubmit}>
-        {formField.map((field, index) => {
-          const { label, type, id, value } = field;
-          return (
-            <div key={index} className="pt-4">
-              <label className="text-gray-900 font-semibold py-2">
-                {id}. {label}-[{value}]
-              </label>
-              <div className="flex">
-                <input
-                  className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-2 focus:border-gray-400 border-gray-200"
-                  type={type}
-                  onChange={(e) =>
-                    setFormField(
-                      formField.map((field) => {
-                        if (field.id === id) return { ...field, value: e.target.value };
-                        return field;
-                      })
-                    )
-                  }
-                  value={value}
-                />
-                <button
-                  className="text-black border-2 border-transparent  hover:border-red-600 w-full bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
-                  onClick={() => removeField(id)}
-                  type="button"
-                >
-                  Remove ❌
-                </button>
-              </div>
-            </div>
-          );
-        })}
+        {formField.map((field, index) => (
+          <FormInputComponent
+            key={index}
+            field={field}
+            onChangeHandler={onChangeHandler}
+            onClickHandler={onClickHandler}
+          />
+        ))}
 
         <div className="pt-4">
           <label className="text-gray-900 font-semibold py-2">Add Field</label>
@@ -111,6 +101,32 @@ function Form(props: IFormFieldProps) {
         </div>
       </form>
     </>
+  );
+}
+
+function FormInputComponent(props: IFormInputComponentProps) {
+  const { field, onClickHandler, onChangeHandler } = props;
+  const { label, type, id, value } = field;
+  return (
+    <div className="pt-4">
+      <label className="text-gray-900 font-semibold py-2">
+        {id}. {label}-[{value}]
+      </label>
+      <div className="flex">
+        <input
+          className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-2 focus:border-gray-400 border-gray-200"
+          type={type}
+          onChange={onChangeHandler}
+          value={value}
+        />
+        <button
+          className="text-black border-2 border-transparent  hover:border-red-600 w-full bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
+          onClick={onClickHandler}
+        >
+          Remove ❌
+        </button>
+      </div>
+    </div>
   );
 }
 
