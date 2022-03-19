2b31448cf3650aecb76c2765f0f009e1045eac93
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-03-19 22:18:38 +0530

    remove: event bug

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,15 +9,13 @@ function Form(props: IFormFieldProps) {
   const [id, setId] = useState(6);
   const [newFieldData, setNewFieldData] = useState<string>("");
 
-  function addNewField(event: React.FormEvent<HTMLButtonElement>) {
-    event.preventDefault();
+  function addNewField() {
     setFormField([...formField, { label: newFieldData, type: "text", id: id, value: "" }]);
     setId(id + 1);
     setNewFieldData("");
   }
 
-  function removeField(event: React.FormEvent<HTMLButtonElement>, id: number) {
-    event.preventDefault();
+  function removeField(id: number) {
     const newformField = formField.filter((f) => f.id !== id);
     setFormField(newformField);
   }
@@ -27,8 +25,7 @@ function Form(props: IFormFieldProps) {
     showResultsCB(formField);
   }
 
-  function clearFormFields(event: React.FormEvent<HTMLButtonElement>) {
-    event.preventDefault();
+  function clearFormFields() {
     setFormField(
       formField.map((field) => {
         return { ...field, value: "" };
@@ -62,7 +59,8 @@ function Form(props: IFormFieldProps) {
                 />
                 <button
                   className="text-black border-2 border-transparent  hover:border-red-600 w-full bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
-                  onClick={(e) => removeField(e, id)}
+                  onClick={() => removeField(id)}
+                  type="button"
                 >
                   Remove ❌
                 </button>
@@ -83,6 +81,7 @@ function Form(props: IFormFieldProps) {
             <button
               className="text-white w-full bg-gray-500 mx-2 px-4 py-2 border-2 border-transparent  hover:border-green-500 rounded-lg hover:bg-gray-600"
               onClick={addNewField}
+              type="button"
             >
               Add New Field ➕
             </button>
@@ -90,20 +89,22 @@ function Form(props: IFormFieldProps) {
         </div>
         <div className="flex justify-between w-full mt-5">
           <button
-            className="text-white w-full bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
+            className="text-white w-full bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent hover:border-black"
             type="submit"
           >
             Submit ✔
           </button>
           <button
-            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
+            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent hover:border-black"
             onClick={closeFormCB}
+            type="button"
           >
             Home 🏠
           </button>
           <button
-            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
+            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent hover:border-black"
             onClick={clearFormFields}
+            type="button"
           >
             Cancel
           </button>
