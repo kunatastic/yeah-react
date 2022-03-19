451f3d03268d3efeb28c5d88e5da641212b73e67
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-03-19 10:45:16 +0530

    use-effect and persistant store

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
-import { formFields } from "../data/FormField";
-import { IFormFieldProps, IFormFields, IFormInputComponentProps } from "../types/forms";
+import React, { useEffect, useState } from "react";
+import { formFields as initialFormField } from "../data/FormField";
+import { IFormFieldProps, IFormFields } from "../types/forms";
+import Fields from "./Fields";
 
 function Form(props: IFormFieldProps) {
   const { closeFormCB, showResultsCB } = props;
 
-  const [formField, setFormField] = useState(formFields);
+  const [formField, setFormField] = useState<IFormFields[]>(initialFormData());
   const [id, setId] = useState(6);
   const [newFieldData, setNewFieldData] = useState<string>("");
+
+  useEffect(() => {
+    console.log("UseEffect hook loaded");
+    document.title = "Awesome Form Editor";
+
+    return () => {
+      document.title = "React App";
+    };
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // saveFormData(,formField);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [formField]);
 
   function addNewField(event: React.FormEvent<HTMLButtonElement>) {
     event.preventDefault();
@@ -37,24 +57,38 @@ function Form(props: IFormFieldProps) {
   }
 
   // TODO: Types
-  const onChangeHandler = (e: any) =>
+  function onChangeHandler(e: any, id: number) {
     setFormField(
       formField.map((field) => {
         if (field.id === id) return { ...field, value: e.target.value };
         return field;
       })
     );
+  }
 
   // TODO: Types
-  const onClickHandler = (e: any) => {
+  function onClickHandler(e: any, id: number) {
     removeField(e, id);
-  };
+  }
+
+  // TODO: types
+  function saveFormData(event: any, currentState: IFormFields[]) {
+    event.preventDefault();
+    localStorage.setItem("kunal-react-form-data", JSON.stringify(currentState));
+    console.log("DATA SET SUCCESFULLY");
+  }
+
+  function initialFormData(): IFormFields[] {
+    const stringifiedFormData = localStorage.getItem("kunal-react-form-data");
+    const formData = stringifiedFormData ? JSON.parse(stringifiedFormData) : initialFormField;
+    return formData;
+  }
 
   return (
     <>
       <form onSubmit={handleFormSubmit}>
         {formField.map((field, index) => (
-          <FormInputComponent
+          <Fields
             key={index}
             field={field}
             onChangeHandler={onChangeHandler}
@@ -81,7 +115,7 @@ function Form(props: IFormFieldProps) {
         </div>
         <div className="flex justify-between w-full mt-5">
           <button
-            className="text-white w-full bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
+            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
             type="submit"
           >
             Submit ✔
@@ -92,41 +126,23 @@ function Form(props: IFormFieldProps) {
           >
             Home 🏠
           </button>
+        </div>
+        <div className="flex justify-between w-full mt-5">
           <button
             className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
             onClick={clearFormFields}
           >
-            Cancel
+            Cancel ✖
+          </button>
+          <button
+            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
+            onClick={(e) => saveFormData(e, formField)}
+          >
+            Save 💾
           </button>
         </div>
       </form>
     </>
-  );
-}
-
-function FormInputComponent(props: IFormInputComponentProps) {
-  const { field, onClickHandler, onChangeHandler } = props;
-  const { label, type, id, value } = field;
-  return (
-    <div className="pt-4">
-      <label className="text-gray-900 font-semibold py-2">
-        {id}. {label}-[{value}]
-      </label>
-      <div className="flex">
-        <input
-          className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-2 focus:border-gray-400 border-gray-200"
-          type={type}
-          onChange={onChangeHandler}
-          value={value}
-        />
-        <button
-          className="text-black border-2 border-transparent  hover:border-red-600 w-full bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
-          onClick={onClickHandler}
-        >
-          Remove ❌
-        </button>
-      </div>
-    </div>
   );
 }
 
