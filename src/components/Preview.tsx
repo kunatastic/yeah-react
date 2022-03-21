44eb87f20122dd8607c302f491731fa706871a63
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-03-21 11:24:50 +0530

    fix: save bug

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -33,6 +33,31 @@ function Preview(props: { formId: string }) {
     setQuery({ questionId: question });
   }, [question]);
 
+  //! Save the Form Data to LocalStorage on every change
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      saveFormData(formField);
+    }, 1000);
+
+    //? Clean timeout on every change of the Form Data
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [formField]);
+
+  //! Save the Form Data to LocalStorage
+  function saveFormData(currentState: IFormData) {
+    const localForms = getLocalForms();
+    const updatedLocalForms = localForms.map((form) => {
+      return form.id === currentState.id ? currentState : form;
+    });
+    saveLocalData(updatedLocalForms);
+  }
+
+  function saveLocalData(currentState: IFormData[]) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(currentState));
+  }
+
   function getLocalForms(): IFormData[] {
     const stringifiedFormData = localStorage.getItem(LOCAL_STORAGE_KEY);
     return stringifiedFormData ? JSON.parse(stringifiedFormData) : [];
