38b20dd406a6732a5a96ee57f82dd1ba5cca2b9c
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-03-21 11:09:35 +0530

    add: preview feature

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -2,28 +2,57 @@ import React from "react";
 import { IFieldProps } from "../types/forms";
 
 function Fields(props: IFieldProps) {
-  const { field, onClickHandler, onChangeHandler } = props;
+  const { field, onClickHandler, onChangeHandler, preview } = props;
   const { label, type, id, value } = field;
   return (
     <div className="pt-4">
-      <label className="text-gray-900 font-semibold py-2">
-        {label} - [{value}]
-      </label>
-      <div className="flex">
-        <input
-          className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-2 focus:border-gray-400 border-gray-200"
-          type={type}
-          onChange={(e) => onChangeHandler(e, id)}
-          value={value}
-        />
-        <button
-          className="text-black border-2 border-transparent  hover:border-red-600 w-full bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
-          onClick={() => onClickHandler(id)}
-          type="button"
-        >
-          Remove ❌
-        </button>
-      </div>
+      {preview ? (
+        <>
+          <label className="text-gray-900 font-semibold py-2">{label}</label>
+          <input
+            className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-2 focus:border-gray-400 border-gray-200"
+            type={type}
+            onChange={(e) => onChangeHandler(e, id)}
+            value={value}
+          />
+        </>
+      ) : (
+        <>
+          <div className="flex justify-between bg-gray-100 p-2">
+            <label className="text-gray-900 font-semibold py-2">{label}</label>
+            <button
+              className="text-black border-2 border-transparent  hover:border-red-600 bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
+              onClick={() => {
+                if (onClickHandler) onClickHandler(id);
+              }}
+              type="button"
+            >
+              Remove ❌
+            </button>
+          </div>
+        </>
+      )}
+      {/* <div className="flex">
+        {preview && (
+          <input
+            className="w-full px-4 py-2 border-2 rounded-lg focus:outline-none focus:border-2 focus:border-gray-400 border-gray-200"
+            type={type}
+            onChange={(e) => onChangeHandler(e, id)}
+            value={value}
+          />
+        )}
+        {!preview && (
+          <button
+            className="text-black border-2 border-transparent  hover:border-red-600 w-full bg-gray-200 mx-2 px-4 py-2 rounded-lg hover:bg-gray-300"
+            onClick={() => {
+              if (onClickHandler) onClickHandler(id);
+            }}
+            type="button"
+          >
+            Remove ❌
+          </button>
+        )}
+      </div> */}
     </div>
   );
 }
---
+++ src/components/Preview.tsx
@@ -0,0 +1,123 @@
+import { Link, navigate, useQueryParams } from "raviger";
+import React, { useEffect, useState } from "react";
+import { formFields as initialFormField } from "../data/FormField";
+import { LOCAL_STORAGE_KEY } from "../config";
+import { IFormData } from "../types/forms";
+import Fields from "./Fields";
+
+function Preview(props: { formId: string }) {
+  const { formId } = props;
+  const [{ questionId }, setQuery] = useQueryParams();
+  const [formField, setFormField] = useState(() => getInitialData(formId));
+  const [question, setQuestion] = useState(0);
+
+  //! Change the title of document if the Form component is rendered
+  useEffect(() => {
+    document.title = formField.title + " Form - Preview";
+
+    //? Cleanup the useEffect hook on unmount of the Form Component
+    return () => {
+      document.title = "React App";
+    };
+  }, [formField.title]);
+
+  useEffect(() => {
+    if (questionId) {
+      setQuestion(Number(questionId));
+    } else {
+      setQuery({ questionId: 0 });
+    }
+  }, []);
+
+  useEffect(() => {
+    setQuery({ questionId: question });
+  }, [question]);
+
+  function getLocalForms(): IFormData[] {
+    const stringifiedFormData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return stringifiedFormData ? JSON.parse(stringifiedFormData) : [];
+  }
+
+  function getInitialData(formId: string): IFormData {
+    const localForms = getLocalForms();
+    const formData = localForms.find((form) => form.id === formId);
+    const newFormData = {
+      id: new Date().getTime().toString(36),
+      title: "",
+      formfields: initialFormField,
+    };
+    if (formData) return formData;
+
+    navigate("/form-do-not-exist", { replace: true });
+    return newFormData;
+  }
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>, id: string) {
+    setFormField({
+      ...formField,
+      formfields: formField.formfields.map((field) => {
+        if (field.id === id) return { ...field, value: e.target.value };
+        return field;
+      }),
+    });
+  }
+
+  return (
+    <>
+      <div className="flex justify-center w-full mt-5">
+        <button
+          className={`text-white w-32 mx-2 px-4 py-2 text-center rounded-lg border-2 border-transparent ${
+            question === 0
+              ? "bg-gray-300 cursor-not-allowed"
+              : "hover:border-black bg-gray-500 hover:bg-gray-600"
+          }`}
+          onClick={() => {
+            if (question !== 0) setQuestion(question - 1);
+          }}
+          type="button"
+        >
+          ◀ Previous
+        </button>
+        <button
+          className={`text-white w-32 mx-2 px-4 py-2 text-center rounded-lg border-2 border-transparent ${
+            question === formField.formfields.length - 1
+              ? "bg-gray-300 cursor-not-allowed"
+              : "hover:border-black bg-gray-500 hover:bg-gray-600"
+          }`}
+          type="button"
+          onClick={() => {
+            if (question !== formField.formfields.length - 1) setQuestion(question + 1);
+          }}
+        >
+          Next ▶
+        </button>
+      </div>
+      <form>
+        <div>
+          <Fields
+            field={formField.formfields[question]}
+            onChangeHandler={onChangeHandler}
+            preview={true}
+          />
+        </div>
+        <div className="flex justify-between w-full mt-5">
+          <Link
+            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 text-center rounded-lg hover:bg-blue-600 border-2 border-transparent  hover:border-black"
+            href={`/result/${formId}`}
+          >
+            Result ✔
+          </Link>
+          <Link
+            className="text-white w-full bg-blue-500 mx-2 px-4 py-2 text-center rounded-lg hover:bg-blue-600 border-2 border-transparent hover:border-black"
+            type="button"
+            href="/"
+          >
+            Home 🏠
+          </Link>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Preview;
