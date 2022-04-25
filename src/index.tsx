5a2e73e50db2e3760431dc96bff9df577dce740f
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-04-25 22:52:48 +0530

    add: netlify configs

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,20 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import LoginProvider from "./util/LoginContext";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
